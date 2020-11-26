425419001689c8070e48ea11998cd19f63715977
malinekenvarn <malin.ekenvarn@gmail.com>
2020-11-26 20:34:06 +0100

    some comments

--- anagram.js
+++ anagram.js
@@ -2,7 +2,8 @@
  Input: two strings. Output: a boolean value. Purpose of this function: To determine if the strings are anagrams of each other.
  My solution: If 2 strings are anagram, they should have the same length. Then  I sort alphabetically and compare.
  I used Array.from() to convert to array to sort, then JSON.stringify() to make them strings again.
- When I compared to how others have solved this, I learned that one can also solve this is with split() and join(). 
+ When I compared to how others have solved this, I learned that one can also solve this is with split() and join().
+ I have to do some research on this subject to learn if split() and join() are a better and faster way. 
 */
 
 
